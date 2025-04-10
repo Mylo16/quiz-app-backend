182d44524d4528ae6efe77396979faee297f4d85
mylo16 <ericopokuantwi384@gmail.com>
2025-04-10 10:24:12 +0000

    Fix line endings in render-build.sh

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
+puts "Requiring application..."
 require_relative "application"
 
 # Initialize the Rails application.
+puts "Initializing Rails app..."
 Rails.application.initialize!
+puts "Initialization done."
